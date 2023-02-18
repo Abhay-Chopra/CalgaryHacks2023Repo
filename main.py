9f4a52bab35f5c973738148149b5c255391e7679
Makiv1 <81124080+Makiv1@users.noreply.github.com>
2023-02-18 14:20:33 -0700

    testing on message

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import discord
 import os
 from secretTokens import TOKEN
-var = 0
+
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user or message:
+        return
+    print("message sent")
+
+
+
 client.run(TOKEN)
-#DFLSFLDJFLKSD
