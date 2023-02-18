59464b6a0a020873923cd334a7b930a007e81d4e
Rohit Nair <87155954+rohitNair21@users.noreply.github.com>
2023-02-18 15:48:19 -0700

    created ical file

--- ical.py
+++ ical.py
@@ -10,7 +10,7 @@ eventCal.add('version', '2.0')
 set(pytz.all_timezones_set)  
 class iCal:
     #date format yyyy/mm/dd, time is MST and 24 hour format
-    def makeEvent(self, name, description, startDate, endDate, startTime, endTime):
+    def makeEvent(self, name, description, startTime, endTime, startDate, endDate ):
         event = Event()
         startD = startDate.split("/")
         if((endDate is None)):
@@ -19,30 +19,24 @@ class iCal:
             endD = endDate.split("/")
         startT = startTime.split(":")
         endT = endTime.split(":")
-       # try:
-        if((startDate is None) or (startTime is None) or (endTime is None)):
-            raise TypeError
-        event.add('name', '{}'.format(name))
-        event.add('description', '{}'.format(description))
-        event.add('dtstart', datetime(int(startD[0]), int(startD[1]), int(startD[2]), int(startT[0]) + int(int(startT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
-        event.add('dtend', datetime(int(endD[0]), int(endD[1]), int(endD[2]), int(endT[0]) + int(int(endT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
-        eventCal.add_component(event)
-       # except TypeError:
-          #  return None
+        try:
+            if((startDate is None) or (startTime is None) or (endTime is None)):
+                raise TypeError
+            event.add('name', '{}'.format(name))
+            event.add('description', '{}'.format(description))
+            event.add('dtstart', datetime(int(startD[0]), int(startD[1]), int(startD[2]), int(startT[0]) + int(int(startT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
+            event.add('dtend', datetime(int(endD[0]), int(endD[1]), int(endD[2]), int(endT[0]) + int(int(endT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
+            eventCal.add_component(event)
+        except TypeError:
+            return None
 
     def makeICal(self):
         directory = Path.cwd() / 'MyCalendar'
         try:
             directory.mkdir(parents=True, exist_ok=False)
         except FileExistsError:
-            print("Folder already exists")
-        else:
-            print("Folder was created")
+            print("Folder exists")
         
         f = open(os.path.join(directory, 'event.ics'), 'wb')
         f.write(eventCal.to_ical())
         f.close()
-
-i = iCal()
-i.makeEvent("Rohit Party","this will be a fun party","2025/06/21","2025/06/21","3:00","4:00")
-i.makeICal()
