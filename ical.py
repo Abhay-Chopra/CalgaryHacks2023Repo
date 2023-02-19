c841f41cb5c29da83d55427f93cec953b6b8d756
AbhayChopra <abhay.chopra1@ucalgary.ca>
2023-02-18 20:48:20 -0700

    Update ical.py to return file so it can be sent off to user group

--- ical.py
+++ ical.py
@@ -10,7 +10,7 @@ eventCal.add('version', '2.0')
 set(pytz.all_timezones_set)  
 class iCal:
     #date format yyyy/mm/dd, time is MST and 24 hour format
-    def makeEvent(self, name, description, startTime, endTime, startDate, endDate ):
+    def make_event(self, name, description, startTime, endTime, startDate, endDate):
         event = Event()
         startD = startDate.split("/")
         if((endDate is None)):
@@ -27,9 +27,13 @@ class iCal:
             event.add('dtstart', datetime(int(startD[0]), int(startD[1]), int(startD[2]), int(startT[0]) + int(int(startT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
             event.add('dtend', datetime(int(endD[0]), int(endD[1]), int(endD[2]), int(endT[0]) + int(int(endT[1])/60), 0, 0, tzinfo=pytz.timezone('Canada/Mountain')))
             eventCal.add_component(event)
+        
         except TypeError:
             return None
-
+        
+        #returns an ical file
+        return eventCal.to_ical()
+    
     def makeICal(self):
         directory = Path.cwd() / 'MyCalendar'
         try:
