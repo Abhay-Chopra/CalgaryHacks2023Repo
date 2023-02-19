bbb82d32a0cdf9bc18573c79f323e0b23c884d4a
Rohit Nair <87155954+rohitNair21@users.noreply.github.com>
2023-02-18 17:16:54 -0700

    Get all channel names

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 client = commands.Bot(command_prefix="!", intents = intents)
+text_name_list = []
+channel_name = ["announcement", "announcements"]
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print("---------------------------------------------")
+    for guild in client.guilds:
+        for channel in guild.text_channels:
+            text_name_list.append(channel.name)
+    
     
 @client.event
 async def on_message(message):
@@ -29,12 +35,12 @@ async def shutdown(ctx):
     print("Shutting down Clubot")
     print("---------------------------------------------")
     await ctx.bot.close()
-
+    
 @client.command()
 async def msg(ctx, user:discord.Member, *, message=None):
     message = "Rohits a bitch"
     embed = discord.Embed(title=message)
     await user.send(embed=embed)
-    
+
 
 client.run(TOKEN)
